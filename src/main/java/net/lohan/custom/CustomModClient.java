cfeafa2e86cccb03846ff425835d0cfc432fb54c
LohanPineau <lohanpineau@gmail.com>
2024-10-20 21:23:52 +0200

    Initial commit

---
+++ src/main/java/net/lohan/custom/CustomModClient.java
@@ -0,0 +1,10 @@
+package net.lohan.custom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/net/lohan/custom/CustomModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lohan.custom;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
